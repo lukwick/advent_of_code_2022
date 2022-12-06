e6628dd8d65040f6e95a45331b6a0097fcbb41d7
lukwick <wick.programming@mailbox.org>
2022-12-06 16:16:08 +0100

    Task 2 completed

--- day1.py
+++ day1.py
@@ -1,8 +1,11 @@
 
+
 from common_func import open_data
 
-def calories_counter(input_data):
 
+# Task 1
+def calories_counter1(input_data):
+    
     calories_temp = 0
     calories_leader = 0
 
@@ -15,7 +18,34 @@ def calories_counter(input_data):
 
     print(calories_leader)
 
+calories_counter1(open_data("day1_input_example.txt"))
+# 24000 is correct
+calories_counter1(open_data("day1_input.txt"))
+# 70296 is correct
 
 
-calories_counter(open_data("day1_input_example.txt")) # 24000 is correct
-calories_counter(open_data("day1_input.txt")) # 70296 is correct
+
+# Task 2
+def calories_counter2(input_data):
+    
+    top3_elves = [0, 0, 0]
+    calories_temp = 0
+
+    for calories in input_data:        
+        if calories > 0:
+            calories_temp += calories
+
+        if calories == 0:
+            if calories_temp > top3_elves[0]:
+                top3_elves[0] = calories_temp
+                top3_elves.sort()
+            calories_temp = 0
+    if calories_temp > top3_elves[0]:
+                top3_elves[0] = calories_temp
+
+    print(top3_elves, top3_elves[0] + top3_elves[1] + top3_elves[2])
+
+calories_counter2(open_data("day1_input_example.txt"))
+# 45000 is correct
+calories_counter2(open_data("day1_input.txt"))
+# 205381 is correct
