82bde4e594815c6a275381aad1390c65276bbc0b
lukwick <wick.programming@mailbox.org>
2022-12-07 21:00:04 +0100

    removed import func

--- functions.py
+++ functions.py
@@ -1,31 +1,6 @@
 
 ## Define functions which I am going to reuse for each exercise
 
-import csv
-
-def open_data(filename, type):
-    """Opens data input file. Returns input data as a list with the type of data you prefer. Places zeros in empty spaces."""
-    
-    # Open file as csv file
-    with open(filename,"r") as file:
-        csvfile = csv.reader(file)
-
-        # Create input list
-        input_data = list()
-        for line in csvfile:
-            input_data.append(line)
-
-        # Adjust list into list with type = integers/floats/strings
-        adjusted_input_data = list()
-        for input in input_data:
-            try:
-                adjusted_input_data.append(type(input[0]))
-            except IndexError:
-                adjusted_input_data.append(type(0))
-                
-    return adjusted_input_data
-
-
 def export_data(day, mode, task, result):
     """Creates data export file to save the results."""
 
