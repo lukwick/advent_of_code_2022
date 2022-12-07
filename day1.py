6cea13e5c1f08b6b75200b402e8a231add4b6633
lukwick <wick.programming@mailbox.org>
2022-12-08 00:07:25 +0100

    rewrite and simplify both functions

--- day1.py
+++ day1.py
@@ -7,7 +7,7 @@ from functions import export_data
 def open_data(filename):
     with open(filename,"r") as file:
         csvfile = csv.reader(file)
-        input_data = list()
+        input_data = []
         for line in csvfile:
             try: 
                 input_data.append(int(line[0]))
@@ -17,75 +17,55 @@ def open_data(filename):
 
 
 # Part One
-def calories_counter1(data_list):
+def calories_counter(data_list):
 
-    # Create counter variables
-    calories_temp = 0
-    calories_leader = 0
+    # Keep track of the calories and calories bags
+    bag = 0
+    bag_list = []
 
-    # Loop through calories list
-    for calories in data_list:  
-
-        # Add calories to temp variable  
-        calories_temp += calories
-
-        # If no calories are found, stop counting
-        if calories == 0:
-            
-            # Check if temp calories are greater than current calories leader
-            if calories_temp > calories_leader:
-                
-                # If so, replace leader
-                calories_leader = calories_temp
-            
-            # Reset calories temp variable
-            calories_temp = 0
+    # Loop through each elf
+    for calories in data_list:
+        if calories > 0:
+            bag += calories
+        else:
+            bag_list.append(bag)
+            bag_list.sort()
+            bag = 0
     
-    # After loop, check if the last calories group is relevant.
-    if calories_temp > calories_leader:
-        calories_leader = calories_temp
+    # Check also the last remaining elf
+    bag_list.append(bag)
+    bag_list.sort()
+    
+    # Return highest value in list
+    return bag_list[-1]
 
-    # Return result
-    return calories_leader
 
-export_data("day1", "1.0:", calories_counter1(open_data("day1_input_example.txt")))
-export_data("day1", "1.1:", calories_counter1(open_data("day1_input.txt")))
+export_data("day1", "1.0:", calories_counter(open_data("day1_input_example.txt")))
+export_data("day1", "1.1:", calories_counter(open_data("day1_input.txt")))
 
 
 # Part Two
 def calories_counter2(data_list):
+
+    # Keep track of the calories and calories bags
+    bag = 0
+    bag_list = []
+
+     # Loop through each elf
+    for calories in data_list:
+        if calories > 0:
+            bag += calories
+        else:
+            bag_list.append(bag)
+            bag_list.sort()
+            bag = 0
     
-    # Create counter variables
-    top3_elves = [0, 0, 0]
-    calories_temp = 0
-
-    # Loop through calories list
-    for calories in data_list:  
-
-        # Add calories to temp variable      
-        calories_temp += calories
-
-        # If no calories are found, stop counting
-        if calories == 0:
-            
-            # Check if temp calories are bigger than smallest value in top3 list
-            if calories_temp > top3_elves[0]:
-
-                # If so, replace first/smallest value with temp calories
-                top3_elves[0] = calories_temp
-
-                # Sort list, so that first position is the smallest again
-                top3_elves.sort()
-
-            # Reset counter variable    
-            calories_temp = 0
-
-    # After loop, check if the last calories group is relevant.
-    if calories_temp > top3_elves[0]:
-        top3_elves[0] = calories_temp
-
-    # Return result
-    return top3_elves, sum(top3_elves)
+    # Check also the last remaining elf
+    bag_list.append(bag)
+    bag_list.sort()
+    
+    # Return the sum of the three highest values in list
+    return sum(bag_list[-3:])
 
 
 export_data("day1", "2.0", calories_counter2(open_data("day1_input_example.txt")))
